b511ab65dcd5646ff8d8baf4523e319e47ffc5a7
rccadungan <rccadungan@student.apc.edu.ph>
2021-10-26 20:01:48 +0800

    Added company logo on login. Created forms for orders.

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,24 +1,10 @@
 @extends('layouts.auth')
 
 @push('css')
-<!--Not Working -->
-.gradient-custom {
-  /* fallback for old browsers */
-  background: #6a11cb;
-
-  /* Chrome 10-25, Safari 5.1-6 */
-  background: -webkit-linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1));
-
-  /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-  background: linear-gradient(to right, rgba(106, 17, 203, 1), rgba(37, 117, 252, 1))
-}
-
+<!--CSS-->
 @endpush
 
 @section('content')
-
-
-
 
 <section class="bg-image"
 style="background-image: url('https://i.pinimg.com/originals/41/4c/18/414c18e3cf6a9ba9a5515d4d5d08b297.png');
@@ -26,17 +12,16 @@ height: 100vh">
   <div class="container py-5 h-100ss">
     <div class="row d-flex justify-content-center align-items-center h-100">
       <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div class="card bg-light text-black" style="border-radius: 1rem;"> 
+        <div class="card bg-white text-black" style="border-radius: 1rem;"> 
           <div class="card-body p-5 text-center">
 
-            <div class="mb-md-5 mt-md-4 pb-5">
-
-              <h2 class="fw-bold mb-2 text-uppercase">Julia's Laundry Management System</h2>
-              <p class="text-black-50 mb-5">Sign with your account</p>
+              <img class="rounded-circle z-depth-3 float-left" alt="Julia's Laundry Logo" src="{{ asset('images/logo_min.jpg') }}" data-holder-rendered="true" width="120" height="140">
+              <h5 class="fw-bold mb-5 text-uppercase text-md-left">Julia's Laundry Management System Version 2</h2>
+              <h4 class="text-black-50 mb-6">Sign with your account</h4>
 
               <div class="form-outline form-black mb-4">
                 <div class="input-group">
-                  <select type="email" id="typeUsernameX" class="form-select form-select-lg w-100 p-2 border border-1 border-secondary rounded rounded-2" aria-label="Select Username">
+                  <select type="email" id="typeUsernameX" class="form-select form-select-lg w-100 p-2 bg-white border border-1 border-secondary rounded rounded-2" aria-label="Select Username">
                     <option selected>Select Username</option>
                     @foreach($users as $user)
                       <option value="{{ $user['username'] }}">{{ $user['username'] }}</option>
@@ -46,7 +31,7 @@ height: 100vh">
               </div>
 
               <div class="form-outline form-black mb-4">
-                <input type="password" id="typePasswordX" class="form-control form-control-lg" placeholder="Password" />
+                <input type="password" id="typePasswordX" class="form-control form-control-lg w-100 p-2 bg-white border border-1 border-secondary rounded rounded-2" placeholder="Password" />
                 <div id="toggle-password" style="position: absolute; right: 0; margin-top: -34px; margin-right: 60px;">
                   <span id="toogle-password-icon" class="fa fa-fw fa-eye"></span>
                 </div>
--- resources/views/orders.blade.php
+++ resources/views/orders.blade.php
@@ -5,7 +5,156 @@
 @endpush
 
 @section('content')
-    this is orders page
+<form>
+  <!-- Order Information Section -->
+  <h5 class="fw-bold mb-2 text-uppercase">Order Information</h2>
+
+  <!-- Service Order Field -->
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="serviceOrderNumber">Service Order No.</label>
+      <input type="number" class="form-control" id="serviceOrderNumber" placeholder="Service Order No.">
+    </div>
+  </div>
+
+  <!-- Received Date Field -->
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="receivedDate">Received Date</label>
+      <input type="date" class="form-control" id="receivedDate" placeholder="Received Date">
+    </div>
+    <!-- Received Time Field -->
+    <div class="form-group col-md-6">
+      <label for="receivedTime">Time</label>
+      <input type="time" class="form-control" id="receivedTime" placeholder="Time">
+    </div>
+  </div>
+
+  <!-- Pickup Date Field -->
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="pickupDate">Pickup Date</label>
+      <input type="date" class="form-control" id="pickupDate" placeholder="Pickup Date">
+    </div>
+    <!-- Pickup Time Field -->
+    <div class="form-group col-md-6">
+      <label for="pickupTime">Time</label>
+      <input type="time" class="form-control" id="pickupTime" placeholder="Time">
+    </div>
+  </div>
+
+  <!-- Customer Information Section -->
+  <h5 class="fw-bold mb-2 text-uppercase">Customer Information</h2>
+
+  <!-- Customer Number Field -->
+  <div class="form-group col-md-6">
+      <label for="customerNumber">Customer No.</label>
+      <input type="number" class="form-control" id="customerNumber" placeholder="Customer No.">
+    </div>
+  </div>
+
+  <!-- Customer Name Field -->
+  <div class="form-group col-md-4">
+      <label for="customerName">Customer Name</label>
+      <select id="customerName" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+
+    <!-- Customer Address Field -->
+    <div class="form-group col-md-4">
+      <label for="address">Address</label>
+      <select id="address" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+
+    <!-- Customer Contact Number Field -->
+    <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="contactNumber">Contact Number</label>
+      <input type="number" class="form-control" id="contactNumber" placeholder="Contact Number">
+    </div>
+    <!-- Make Customer VIP Option -->
+    <div class="form-group col-md-4">
+      <label for="VIP">VIP</label>
+      <select id="VIP" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+
+    <!-- Customer TransactionRemarks Field -->
+    <div class="form-group col-md-6">
+      <label for="remarks">Remarks</label>
+      <input type="textArea" class="form-control" id="remarks" placeholder="Remarks">
+    </div>
+
+    <!-- Order Details Section -->
+    <h5 class="fw-bold mb-2 text-uppercase">Order Details</h2>
+
+    <div class="form-group col-md-6">
+    <!-- Add New Item Button -->
+    <button type="submit" class="btn btn-primary">Add a New Item</button>
+    <!-- Articles Section -->
+      <label for="article">Article</label>
+      <input type="email" class="form-control" id="article" placeholder="Article">
+    </div>
+  
+  <!-- Order Quantity Field -->
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="quantity">Quantity</label>
+      <input type="number" class="form-control" id="quantity" placeholder="Quantity">
+    </div>
+    <!-- Order Discount Field -->
+    <div class="form-group col-md-4">
+      <label for="discount">Discount</label>
+      <select id="discount" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+
+    <!-- Payment Details Section -->
+    <h5 class="fw-bold mb-2 text-uppercase">Payment</h2>
+    <!-- Payment Option -->
+    <div class="form-group col-md-4">
+      <label for="paymentOption">Payment Option</label>
+      <select id="paymentOption" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+
+    <!-- Payment Channel Option -->
+    <div class="form-group col-md-4">
+      <label for="paymentChannel">Payment Channel</label>
+      <select id="paymentChannel" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+
+    <!-- Enter Amount Field -->
+    <div class="form-group col-md-6">
+      <label for="amount">Amount</label>
+      <input type="number" class="form-control" id="amount" placeholder="Amount">
+    </div>
+
+    <!-- Cancel Button -->
+    <button type="submit" class="btn btn-primary">Cancel</button>
+    <!-- Save Button -->
+    <button type="submit" class="btn btn-primary">Save</button>
+    <!-- Send Button -->
+    <button type="submit" class="btn btn-primary">Send</button>
+    <!-- Print Button -->
+    <button type="submit" class="btn btn-primary">Print</button>
+
+  </div>
+</form>
 @endsection
 
 @push('scripts')
